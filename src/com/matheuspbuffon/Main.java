b9cdb22564a8ed4ef55f007be121f3092cab09d3
Matheus P. Buffon <matheuspbuffon@gmail.com>
2020-03-05 12:44:40 -0300

    Update 03

--- src/com/matheuspbuffon/Main.java
+++ src/com/matheuspbuffon/Main.java
@@ -6,15 +6,27 @@ public class Main {
         Car benz = new Car(10.54, 30000, 4);
         Car audi = new Car();
 
-        System.out.println("Benz has "+ benz.doors +" doors");
-        System.out.println("Benz is priced at "+ benz.price +" dollars");
-        System.out.println("The mileage being: "+ benz.mileage);
+        DirtCar dirtyBenz = new DirtCar();
+        dirtyBenz.setMileage(6);
+        dirtyBenz.setDoors(2);
+        dirtyBenz.setPrice(250000);
+        dirtyBenz.setDirtyTerrainTopSpeed(45);
+
+        System.out.println(dirtyBenz.startDirtCar());
+        System.out.println("Dirty Benz's mileage is "+ dirtyBenz.getMileage());
+        System.out.println("Dirty Benz's doors count is "+ dirtyBenz.getDoors());
+        System.out.println("Dirty Benz's price is "+ dirtyBenz.getPrice());
+        System.out.println("Dirty Benz's terrain top speed is "+ dirtyBenz.getDirtyTerrainTopSpeed());
+
+        /*System.out.println("Benz has "+ benz.getDoors() +" doors");
+        System.out.println("Benz is priced at "+ benz.getPrice() +" dollars");
+        System.out.println("The mileage being: "+ benz.getMileage());
 
         benz.carStart();
         benz.carShutOff();
 
-        System.out.println("Audi has "+ audi.doors +" doors.");
-        System.out.println("Audi is priced at: "+ audi.price +" dollars.");
-        System.out.println("The mileage being: "+ audi.mileage);
+        System.out.println("Audi has "+ audi.getDoors() +" doors.");
+        System.out.println("Audi is priced at: "+ audi.getPrice() +" dollars.");
+        System.out.println("The mileage being: "+ audi.getMileage());*/
     }
 }
