643f953c57586899b70152dbd024ddc716b5b049
Matheus P. Buffon <matheuspbuffon@gmail.com>
2020-03-05 01:24:08 -0300

    Update 01

--- src/com/matheuspbuffon/Car.java
+++ src/com/matheuspbuffon/Car.java
@@ -5,4 +5,18 @@ public class Car {
     double mileage;
     int price;
     int doors;
+
+    public Car(double mileage, int price, int doors) {
+        this.mileage = mileage;
+        this.price = price;
+        this.doors = doors;
+    }
+
+    public void carStart() {
+        System.out.println("Car Started");
+    }
+
+    public void carShutOff() {
+        System.out.println("Car Engine Off");
+    }
 }
